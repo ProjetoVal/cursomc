bd5fb7b41872f897b2589a75d232c0529e80d5cd
Valdenilson da Silva Barbosa <vava20br@gmail.com>
2026-07-22 10:10:35 -0300

    criando instanciacao na class principal pro banco h2

--- src/main/java/com/empresa/cursomc/CursomcApplication.java
+++ src/main/java/com/empresa/cursomc/CursomcApplication.java
@@ -1,14 +1,35 @@
 package com.empresa.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.empresa.cursomc.domain.Categoria;
+import com.empresa.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+  
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+//
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Hopitalar");
+		Categoria cat4 = new Categoria(null, "Infantil");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4));
 	}
 
 }
